a73893e76697f1599a2536c01d9762965dc1862b
Emmanuel Tweneboah <macbook@MacBooks-MacBook-Pro.local>
2020-08-31 15:19:47 +0800

    Importing files

---
+++ 3-importing-own-file/app.js
@@ -0,0 +1,10 @@
+const utils = require('./utils');
+const name = require('./utils');
+
+//This is the concept of creating your own function and use it in another file
+
+// As soon as the file is required to this file and this file runs the function runs automatically
+
+//Each file has their own scope
+
+console.log(name);
---
+++ 3-importing-own-file/utils.js
@@ -0,0 +1,7 @@
+console.log('From Utils.js file');
+
+const name = 'Emma';
+
+//Before you can use this in another file you need to exports
+
+module.exports = name;
